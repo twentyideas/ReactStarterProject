95cc480739cf5237db09b1787fe47824d423408e
Greg McKelvey <mckelveygreg@gmail.com>
2020-02-28 14:47:36 -0800

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     extends: ["react-app", "plugin:prettier/recommended", "plugin:react-directives/recommended"],
     rules: {
+        "prettier/prettier": "warn",
         "react-directives/no-unused-vars": "off", // this doubles up
         "object-curly-newline": ["error", { multiline: true, consistent: true }],
         "newline-per-chained-call": "off",
--- craco.config.js
+++ craco.config.js
@@ -5,9 +5,6 @@ const { ESLINT_MODES } = require("@craco/craco")
 // const { when, whenDev, whenProd, whenCI, whenTest, ESLINT_MODES, POSTCSS_MODES } = require("@craco/craco");
 module.exports = {
     reactScriptsVersion: "react-scripts",
-    eslint: {
-        mode: ESLINT_MODES.file
-    },
     babel: {
         plugins: [["react-directives", { prefix: "x", pragmaType: "React" }]]
     }
